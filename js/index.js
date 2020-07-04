d153aad4b1ea28cdf757074ff9235d4f7e67eeb9
Alex Oliveira <alex_oliveira08@yahoo.com.br>
2020-07-03 23:51:49 -0300

    Incluido chamada API para teste

--- js/index.js
+++ js/index.js
@@ -1,1 +1,28 @@
-console.log("Teste Js")
+let resultado_div = document.querySelector('div .resultado')
+
+let input = document.querySelector('input')
+// let button = document.querySelector('button')
+
+//Função busca artista
+
+function bucarArtista () {
+
+    let url = `https://api.vagalume.com.br/search.art?q=${input.value}&limit=5`
+    
+    fetch(url)
+        .then(res => {
+            return res.json()
+        })
+        .then(data => {
+            console.log(data.response.docs[0].band)
+        })
+        .catch(err => {
+            err = "erro aqui"
+            return err
+        })
+
+    console.log(url) 
+ 
+}
+ 
+
